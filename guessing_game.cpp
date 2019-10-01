95c37d824e69787484860ff535a94cd759331315
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-10-01 16:55:42 +0000

    Added guessing game

---
+++ guessing_game.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 St. Mother Teresa HS All Rights Reserved.
+//
+// Created By:Ben Lapuhapo.
+// Created on:Sep 2019.
+// This program is a guessing game
+
+#include <iostream>
+
+int main() {
+    // This function checks if the user's number is correct
+    int user_input;
+    int secret_number = rand_r() % 1 + 2;
+
+    // input
+    std::cout << "Enter Your guess from (0-9): ";
+    std::cin >> user_input;
+    std::cout << "" << std::endl;
+    // process
+
+    if (user_input == secret_number) {
+    std::cout << "Correct Number ---->";
+    std::cout << secret_number;
+    } else {
+    std::cout << "Wrong Number.";
+    }
+  return 0;
+}
